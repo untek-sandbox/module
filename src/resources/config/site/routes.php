f6da2f1b63c9f419899b5626080096409e055d1e
vitaliy <theyamshikov@yandex.ru>
2024-01-17 21:11:34 +0600

    upd

--- src/resources/config/site/routes.php
+++ src/resources/config/site/routes.php
@@ -5,7 +5,6 @@ use Untek\Sandbox\Module\Application\Services\RouteGenerator;
 use Untek\Sandbox\Module\Presentation\Http\Site\Controllers\MainPageController;
 
 return function (RoutingConfigurator $routes) {
-    // SANDBOX_NAMESPACES=Forecast\Map\Sandbox,Untek\Sandbox\Crypt
     $namespaces = explode(',', getenv('SANDBOX_NAMESPACES'));
     (new RouteGenerator())->generate($namespaces, $routes);
 
