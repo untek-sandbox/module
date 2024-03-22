c5b8da6bacd6aa775f2bd5cd781e60d65589b706
Vitaliy <theyamshikov@yandex.ru>
2024-03-22 18:20:09 +0500

    upd

--- src/Modules/Demo/Presentation/Http/Site/Controllers/WorkoutController.php
+++ src/Modules/Demo/Presentation/Http/Site/Controllers/WorkoutController.php
@@ -2,10 +2,10 @@
 
 namespace Untek\Sandbox\Module\Modules\Demo\Presentation\Http\Site\Controllers;
 
-use Untek\Core\Text\Helpers\TextHelper;
-use Untek\Sandbox\Module\Presentation\Http\Site\Controllers\AbstractSandboxController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Untek\Core\Text\Helpers\TextHelper;
+use Untek\Sandbox\Module\Presentation\Http\Site\Controllers\AbstractSandboxController;
 
 class WorkoutController extends AbstractSandboxController
 {
@@ -19,7 +19,7 @@ class WorkoutController extends AbstractSandboxController
             0.5,
         ];
         $custom = [
-            [
+            /*[
                 2.5,
                 2.5,
                 2.5,
@@ -48,15 +48,34 @@ class WorkoutController extends AbstractSandboxController
                 2.5,
                 2.5,
                 2.5,
-            ],
+            ],*/
         ];
-        $arr = $this->generateTable($weight, 1.5, $custom);
+        $maxItems = 4;
+        $arr = $this->generateTable($weight, 1.5, $custom, $maxItems);
+        $forTwo = $this->renderTable($arr);
+
+        $weight = [
+            2.5,
+            2.5,
+            2.5,
+            2.5,
+            1.25,
+            1.25,
+            0.5,
+        ];
+        $maxItems = 5;
+        $arrOne = $this->generateTable($weight, 1.5, $custom, $maxItems, 15);
+        $forOne = $this->renderTable($arrOne);
+
+
+
         return $this->renderDefault([
-            'content' => $this->renderTable($arr),
+            'content' => "<h2>Two</h2>$forTwo<h2>One</h2>$forOne", // $this->renderTable($arr),
         ]);
     }
 
-    private function generateTable(array $weight, float $barWeight, array $custom): array {
+    private function generateTable(array $weight, float $barWeight, array $custom, int $maxItems, float $from = 0): array
+    {
         $arr = [];
         $countWeight = count($weight);
         $count = pow(2, $countWeight);
@@ -68,11 +87,18 @@ class WorkoutController extends AbstractSandboxController
                 $bits[$index] = intval($bit);
             }
             $item = [];
+            $itemsCount = 0;
             foreach ($weight as $index => $weightItem) {
-                $item[] = $bits[$index] * $weight[$index];
+                $val = $bits[$index] * $weight[$index];
+                $item[] = $val;
+                if($val) {
+                    $itemsCount++;
+                }
             }
             $sum = $this->sum($item, $barWeight);
-            $arr[$sum] = $item;
+            if($itemsCount <= $maxItems && $sum > $from) {
+                $arr[$sum] = $item;
+            }
         }
 
         foreach ($custom as $item) {
@@ -80,15 +106,18 @@ class WorkoutController extends AbstractSandboxController
             $arr[$sum] = $item;
         }
 
+
         return $arr;
     }
 
-    private function sum(array $item, float $barWeight): string {
+    private function sum(array $item, float $barWeight): string
+    {
         $sum = array_sum($item) * 2 + $barWeight;
         return strval($sum);
     }
 
-    private function renderTable(array $arr): string {
+    private function renderTable(array $arr): string
+    {
         $table = '';
         $table .= '<table class="table table-striped table-bordered" style="width: 300px">';
         foreach ($arr as $sum => $row) {
